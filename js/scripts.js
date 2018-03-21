3cb282b37ab4537b1d329461560334ac36ca1587
Wendy Anderson-Burton <wendyanderson-burton@wendys-air.attlocal.net>
2018-03-20 22:22:57 -0700

    update files

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,23 @@
-$( document ).ready( function () {
+
+$( document ).ready(function () {
 
   console.log("Hello world!");
 
+  $("#article p a img").click(function (event) {
+    event.preventDefault();
 
-  
+    $("#lightbox").show();
+
+    var imageLink = $(this).attr("src");
+
+    var imgText = "<img src='" + imageLink + "' />";
+
+    $("#lightbox").html(imgText);
+
+    $("#lightbox").click(function (event) {
+      $("#lightbox").hide();
+
+    });
+
+  });
+});
